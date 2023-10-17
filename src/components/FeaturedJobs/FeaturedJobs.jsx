9e460de54d9611fce128825d749cfc2a3fe1bcf7
Anik <clananik@gmail.com>
2023-10-16 22:36:50 -0700

    again done

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,12 +4,29 @@ import Jobs from "../Jobs/jobs";
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
   const [datalength, setDatalength] = useState(6);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(jobs);
+    } else if (filter === "remote") {
+      const remoteJobs = jobs.filter((job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = jobs.filter((job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
+  console.log(displayJobs);
 
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
-      .then((data) => setJobs(data));
+      .then((data) => {setJobs(data),setDisplayJobs(data)});
   }, []);
+
   return (
     <div className="py-28">
       <div className="text-center">
@@ -19,14 +36,47 @@ const FeaturedJobs = () => {
           need. Its your future
         </p>
       </div>
+
+      {/* ------------------ */}
+      <div className="flex justify-end pe-10 mb-14">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn m-1">
+            Filter By
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+          >
+            <li onClick={() => handleJobsFilter("all")}>
+              <a>All</a>
+            </li>
+            <li onClick={() => handleJobsFilter("remote")}>
+              <a>Remote</a>
+            </li>
+            <li onClick={() => handleJobsFilter("onsite")}>
+              <a>Onsite</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* ---------------------------- */}
+
       <div className="grid grid-cols-3 gap-6 mx-5 justify-items-center">
-        {jobs.slice(0, datalength).map((job) => (
+        { displayJobs.slice(0, datalength).map((job) => (
           <Jobs key={job.id} job={job}></Jobs>
         ))}
+        {/* {
+          displayJobs.map(job=> <Jobs key={job.id} ></Jobs>)
+        } */}
       </div>
       <div className="text-center mt-8">
         <div className={datalength === jobs.length ? "hidden" : ""}>
-          <button onClick={() => setDatalength(jobs.length)} className="btn btn-primary button border-0">Show All</button>
+          <button
+            onClick={() => setDatalength(jobs.length)}
+            className="btn btn-primary button border-0"
+          >
+            Show All
+          </button>
         </div>
       </div>
     </div>
